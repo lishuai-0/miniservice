4b7d589fbd247d592a819a95529d25cdbd2b1027
lishuai-0 <74478031+lishuai-0@users.noreply.github.com>
2021-06-10 15:17:19 +0800

    为啥提交不上去啊

--- ribbon-consumer/src/main/java/com/example/ribbonconsumer/RibbonConsumerApplication.java
+++ ribbon-consumer/src/main/java/com/example/ribbonconsumer/RibbonConsumerApplication.java
@@ -14,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 public class RibbonConsumerApplication {
 
+
     @Bean
     @LoadBalanced
     RestTemplate restTemplate(){
